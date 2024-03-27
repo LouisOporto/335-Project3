c99e4956ac08b7e40758bc629ac0c0286aa9d82a
Louis Oporto <louisoporto042@csu.fullerton.edu>
2024-03-27 16:02:41 -0700

    Swapped to cpp instead of py

---
+++ algorithm1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// Exhaustive Search
+
+int main() {
+
+  return 0;
+}
--- algorithm1.py
+++
@@ -1,3 +0,0 @@
-# Temporary python, switch to c++ if groups wants to do C++ instead
-
-#Exhaustive Search

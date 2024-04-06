d9ec6940e50b7178c832558a190185534e5a6cc7
Louis Oporto <louisoporto042@csu.fullerton.edu>
2024-04-05 22:52:45 -0700

    Added pseudocode

--- algorithm1.cpp
+++ algorithm1.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
 // Exhaustive Search
+/* 
+soccer_exhaustive(G):
+  len = 𝑟 + 𝑐 − 2
+  counter = 0
+  for bits from 0 to 2^𝑙𝑒𝑛 − 1 inclusive:
+    candidate = empty list of moves
+    for k from 0 𝑡𝑜 𝑙𝑒𝑛 − 1 inclusive:
+      bit = (bits >> 𝑘) & 1
+      if bit == 1:
+        candidate.add(→)
+      else:
+        candidate.add(↓)
+    if candidate stays inside the grid, never crosses an X cell, and ends at (𝑟 − 1, 𝑐 − 1):
+      counter++
+  return counter
+ */
 
 int main() {
 
--- algorithm2.cpp
+++ algorithm2.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
 //Dynamic Programming
+/* 
+soccer_dyn_prop(F):
+  //corner case: inital cell is impassible
+  if F[0][0] == X:
+    return 0
+  A = new rXc matrix intialized to zeros
+  //base case
+  A[0][0] = 1
+  //general cases
+  for i from 0 to r - 1 inclusive:
+    for j from 0 to c - 1 inlcude:
+      if F[i][j] == X:
+        A[i][j] = 0
+        continue
+      above = from_left = 0
+      if i > 0 and F[i - 1][j] == .:
+        above = A[i - 1][j]
+      if j > 0 and F[i][j - 1] == .:
+        left = A[i][j - 1]
+      A[i][j] += above + left
+  return A[r - 1][c - 1]
+*/
 
 int main() {
   
